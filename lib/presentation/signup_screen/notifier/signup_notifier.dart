b97725ac861ffac8ae7efe7c89af0016a9109678
RojiyaM <rojiyamunikar@gmail.com>
2024-05-16 00:16:44 +1000

    fix: file imports

--- lib/presentation/signup_screen/notifier/signup_notifier.dart
+++ lib/presentation/signup_screen/notifier/signup_notifier.dart
@@ -1,9 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:equatable/equatable.dart';
-import 'package:money_mgmt/core/network/apis.dart';
-import 'package:money_mgmt/core/network/logger.dart';
-import 'package:money_mgmt/core/utils/flash_message.dart';
+import 'package:oil2u/core/network/apis.dart';
+import 'package:oil2u/core/network/logger.dart';
+import 'package:oil2u/core/utils/flash_message.dart';
 import '../../../core/app_export.dart';
 import '../../../core/network/api_s.dart';
 import '../models/signup_model.dart';
